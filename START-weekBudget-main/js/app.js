e879534dc2912ae992bd7183b099a8e1728c53f0
Sherrelyn <>
2021-10-30 15:10:36 +0800

    Added classes to the amount of budget. Added variables.

--- START-weekBudget-main/js/app.js
+++ START-weekBudget-main/js/app.js
@@ -1,12 +1,38 @@
 //Classes
+
+class Budget {
+    constructor(budget) {
+        this.budget = Number (budget);
+        this.budgetLeft = this.budget;
+
+    }
+}
  
-	 
+// Everything related to HTML
+	class HTML{
+
+        // Inserts the budget when the user submits iy
+        insertBudget(amount) {
+            console.log(amount);
+            // Insert into HTML
+            budgetTotal.innerHTML = `${amount}`;
+            budgetLeft.innerHTML = `${amount}`;
+
+        }
+
+    }
  
 	 
 //Variables
-    const addExpenseForm = document.querySelector('#add-expense');
+    const addExpenseForm = document.querySelector('#add-expense'),
+   budgetTotal = document.querySelector('span#total'),
+   budgetLeft = document.querySelector('span#left');
 	
     let budget, userBudget;
+
+    // Instanciate the HTML CLASS
+    const html = new HTML()
+
 	 
 	
 	 
@@ -22,13 +48,22 @@
         // validate the user budget
         if(userBudget === null || userBudget === '' || userBudget === '0') {
             window.location.reload();
+        } else {
+            // Budget is valid then instanciate the budget class
+            budget = new Budget(userBudget);
+
+            // Instanciate HTML Class
+            html.insertBudget(budget.budget);
+
+
         }
 
     });
 
 // When a new expense is added
-    addExpenseForm.eventListeners('submit', function(e) {
-        e.preventDefault();
+addExpenseForm.addEventListener('submit', function(e) {
+    e.preventDefault();
 
-    });
+});
+
 }
